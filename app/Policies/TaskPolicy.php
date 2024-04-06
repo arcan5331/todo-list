ec2a6023161160ceed6dd95ad99a0344ea76d44b
arcan5331 <alighaforian@yahoo.com>
2024-04-06 10:05:31 +0330

    added  TaskPolicy.

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    public function view(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function delete(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function restore(User $user, Task $task): bool
+    {
+        return false;
+    }
+
+    public function forceDelete(User $user, Task $task): bool
+    {
+        return false;
+    }
+}
