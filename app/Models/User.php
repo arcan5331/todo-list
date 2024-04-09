574c7d917ba03bb1aa741acd406ec2345417dc1a
arcan5331 <alighaforian@yahoo.com>
2024-04-09 21:27:42 +0330

    added the tag model and its relation with the user model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Task::class);
     }
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
 }
